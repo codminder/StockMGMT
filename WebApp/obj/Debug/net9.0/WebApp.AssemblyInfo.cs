48930ff317a8884e9bfd47ea6f576a51f8774b01
Wlad Roor <w.k.roor@gmail.com>
2025-06-03 11:45:37 +0200

    added some files

--- WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95f31c6e87e12374e55354a2b1b252b503fbde94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba019c14f37e8157ae8bb11b78cf1879adc078af")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
