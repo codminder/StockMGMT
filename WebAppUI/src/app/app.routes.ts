8ba9f9de9ed12a8ff0badd7bdfca7d7269bc69b8
Wlad Roor <w.k.roor@gmail.com>
2025-06-16 14:46:16 +0200

    last commit

--- WebAppUI/src/app/app.routes.ts
+++ WebAppUI/src/app/app.routes.ts
@@ -2,9 +2,12 @@
 import { Routes } from '@angular/router';
 import { ProductComponent } from './platform/products/products.component';
 import { LoginComponent } from './authentication/login/login.component';
+import { Platform } from '@angular/cdk/platform';
+import { PlatformComponent } from './platform/platform.component';
 
 export const routes: Routes = [
-    {path: '/platform/products', component: ProductComponent},
+    {path: '', component: PlatformComponent},
+    {path: '/products/products', component: ProductComponent},
     {path: 'login', component: LoginComponent},
     /*{path: 'customers', component: CustomerComponent},*/
 ];
