b8c8e07397b3e7c2a83225edb490b0f4d290d291
Wlad Roor <w.k.roor@gmail.com>
2025-06-27 14:33:53 +0200

    last mod

--- WebApp/Services/UserService.cs
+++ WebApp/Services/UserService.cs
@@ -1,5 +1,9 @@
+
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Tokens;
 using WebApp.Interfaces.Repositories;
 using WebApp.Interfaces.Services;
 using WebApp.Models;
@@ -8,22 +12,24 @@ namespace WebApp.Services;
 
 public class UserService : IUserService
 {
-    private readonly IUserRepository _repository;
-    public UserService(IUserRepository repository, IHttpContextAccessor IHttpContextAccessor)
+    private readonly IUserRepository _userRepository;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    public UserService(IUserRepository repository, IHttpContextAccessor httpContextAccessor)
     {
-        _repository = repository;
+        _userRepository = repository;
+        _httpContextAccessor = httpContextAccessor;
     }
 
     public User? Login(string email, string password)
     {
-        var user = _repository.GetByEmail(email);
+        var user = _userRepository.Get(email);
 
         if (user != null)
         {
             if (user.Password == password)
             {
                 user.LastLoginDate = DateTime.UtcNow;
-                _repository.UpdateUser(user);
+                _userRepository.Update(user);
 
                 return user;
             }
@@ -34,7 +40,42 @@ public class UserService : IUserService
     public User GetCurrentUser()
     {
         var contextUser = _httpContextAccessor.HttpContext?.User;
+        var userIdClaim = contextUser?.FindFirst("id");
 
-        var userClaim = contextUser?.FindFirst(ClaimTypes.NameIdentifier);
+        if (userIdClaim == null)
+        {
+            throw new UnauthorizedAccessException("User id not found in token.");
+        }
+
+        int userId = int.Parse(userIdClaim.Value);
+
+        var userResult = _userRepository.Get(userId);
+
+        if (userResult == null)
+        {
+            throw new NullReferenceException($"user with id ${userId} not found in the database.");
+        }
+        return userResult;
+    }
+
+    public JwtSecurityToken GetToken(User user)
+    {
+        var claims = new[]
+        {
+            new Claim("email", user.Email),
+            new Claim("id", user.Id.ToString())
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("DitIsSuperSecretPlusZestienKarakters"));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: "StockManagement",
+            audience: "StockManagementUI",
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: creds);
+
+        return token;
     }
 }
