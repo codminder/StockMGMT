6a4041d1fc0b5b4d3c3dfb2bc14fcb5fc7d2a9fd
codminder <w.k.roor@gmail.com>
2025-07-05 12:02:42 +0200

    last

--- WebAppUI/src/app/core/services/user.service.ts
+++ WebAppUI/src/app/core/services/user.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from "@angular/core";
 import { jwtDecode } from "jwt-decode";
+import { Router } from '@angular/router';
+import { User } from '../dataContracts/userModel';
+
 
 @Injectable({
     providedIn: 'root',
@@ -7,25 +10,27 @@ import { jwtDecode } from "jwt-decode";
 
 
 export class UserService {
-    constructor(private router: Rouer) { }
+    constructor(private router: Router) { }
 
     public getUser(): User {
         const token = localStorage.getItem('token');
+    
+
+        if (token) {
+            const user = jwtDecode(token);
+            return user as User;
+        }
+        this.router.navigate(['/login']);
+        return {} as User;
     }
 
-    if (token) {
-        const user = jwtDecode(token);
-        return user as User;
+    public getTokenTimeValid(): number {
+        const token = localStorage.getItem('token');
+        if (token) {
+            const decodedToken: any = jwtDecode(token);
+            return decodedToken.exp ? decodedToken.exp - Math.floor(Date.now()/1000) : 0;
+        }
+        return 0;
     }
-    this.router.navigate(['/login']);
-    return {} as User;
-}
 
-public getTokenTimeValid(): number {
-    const token = localStorage.getItem('token');
-    if (token) {
-        const decodedToken: any = jwtDecode(token);
-        return decodedToken.exp? decodedToken.exp - Math.floor(Date.floor(Date.now()/1000) : 0);
-    }
-    return 0;
 }
