c68dbc18e22bbf00e43a23916e7420f04d66af79
codminder <w.k.roor@gmail.com>
2025-07-16 21:53:58 +0200

    last good commit

---
+++ WebAppUI/src/app/authentication/register/register.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { RegisterDto } from '../../core/dataContracts/registerDto';
+import { validateHorizontalPosition } from '@angular/cdk/overlay';
+
+@Component({
+  selector: 'app-register',
+  imports: [CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss'
+})
+export class RegisterComponent {
+  submitted = false;
+  registerForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, { validators: this.passwordMatchValidator.bind(this) });
+  }
+
+  get f () {
+    return this.registerForm.controls;
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+
+    return password === confirmPassword ? null : { mismatch: true };
+  }
+
+  register() {
+    if (this.registerForm.valid) {
+      const registerDto: RegisterDto = this.registerForm.value;
+
+      this.authService.register(registerDto).subscribe({
+        next: (result: any) => {
+          localStorage.setItem('token', result.token);
+          this.router.navigate(['/platform/products']);
+        },
+        error: (err: string) => {
+          console.log('Register failed', err);
+        }
+      });
+    }
+  }
+
+}
